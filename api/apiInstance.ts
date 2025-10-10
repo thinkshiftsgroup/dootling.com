bf0bedbb61451121e57e86667f493bb4bd79817b
denzubechi <chukwumasamuel371@gmail.com>
2025-10-10 11:16:53 +0100

    feat:created profile store

--- api/apiInstance.ts
+++ api/apiInstance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const API_BASE_URL = "https://api.dootling.com";
 
@@ -8,5 +9,20 @@ const apiInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+apiInstance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("dootling_auth_token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default apiInstance;
---
+++ stores/useProfileStore.ts
@@ -0,0 +1,72 @@
+import { create } from "zustand";
+
+interface UserProfile {
+  id: string;
+  email: string;
+  username: string;
+  fullName: string;
+  firstname: string | null;
+  lastname: string | null;
+  userType: string;
+  isVerified: boolean;
+}
+
+interface Biodata {
+  dateOfBirth: Date | string;
+  country: string | null;
+  pronouns: string | null;
+  phone: string | null;
+  state: string | null;
+  role: string | null;
+  industry: string | null;
+  tags: string | null;
+  headline: string | null;
+}
+
+interface ProfileState {
+  profile: (UserProfile & Biodata) | null;
+  isLoading: boolean;
+  isLoaded: boolean;
+
+  setProfile: (profileData: UserProfile & Biodata) => void;
+  clearProfile: () => void;
+  setLoading: (loading: boolean) => void;
+  setLoaded: (loaded: boolean) => void;
+
+  updatePartialProfile: (data: Partial<UserProfile & Biodata>) => void;
+}
+
+export const useProfileStore = create<ProfileState>((set) => ({
+  profile: null,
+  isLoading: false,
+  isLoaded: false,
+
+  setLoading: (loading) => set({ isLoading: loading }),
+  setLoaded: (loaded) => set({ isLoaded: loaded }),
+
+  setProfile: (profileData) =>
+    set({
+      profile: profileData,
+      isLoaded: true,
+      isLoading: false,
+    }),
+
+  clearProfile: () =>
+    set({
+      profile: null,
+      isLoaded: false,
+      isLoading: false,
+    }),
+
+  updatePartialProfile: (data) =>
+    set((state) => {
+      if (!state.profile) return state;
+
+      return {
+        profile: {
+          ...state.profile,
+          ...data,
+        },
+      };
+    }),
+}));
