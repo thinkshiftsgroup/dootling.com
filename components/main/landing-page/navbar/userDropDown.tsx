37ff476be95e874eb43053d251089c4bc35d5a75
denzubechi <chukwumasamuel371@gmail.com>
2025-10-11 02:55:53 +0100

    feat:implemented signout

--- components/main/landing-page/navbar/userDropDown.tsx
+++ components/main/landing-page/navbar/userDropDown.tsx
@@ -4,10 +4,22 @@ import Image from "next/image";
 import { Dropdown } from "./dropDown";
 import { User, UserGear, Gear, LockSimple, SignOut } from "phosphor-react";
 import { useRouter } from "next/navigation";
-import user from "@/public/images/user/userImg.jpg"
+import user from "@/public/images/user/userImg.jpg";
+
+import Cookies from "js-cookie";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { toast } from "sonner";
 
 export default function UserDropdown() {
   const router = useRouter();
+  const logoutUser = useAuthStore((state: any) => state.logout);
+  const handleSignOut = () => {
+    Cookies.remove("dootling_auth_token");
+    logoutUser();
+    toast.info("You have been signed out.");
+    router.push("/");
+  };
+
   return (
     <Dropdown
       trigger={
@@ -17,7 +29,6 @@ export default function UserDropdown() {
             width={35}
             height={35}
             alt="user"
-            // priority
             className="rounded-sm mt-1.5 cursor-pointer"
           />
         </div>
@@ -33,16 +44,6 @@ export default function UserDropdown() {
               <User size={20} className="text-gray-700" />
               <a className="text-base text-gray-800 font-normal">My Profile</a>
             </div>
-
-            {/* <div
-              onClick={() => router.push("/profile/edit-profile")}
-              className="flex items-center gap-2 py-2 cursor-pointer hover:bg-gray-50 rounded-md"
-            >
-              <UserGear size={20} className="text-gray-700" />
-              <a className="text-base text-gray-800 font-normal">
-                Edit Profile
-              </a>
-            </div> */}
 
             <div
               onClick={() => router.push("/profile/profile-settings")}
@@ -63,10 +64,11 @@ export default function UserDropdown() {
                 Privacy Settings
               </a>
             </div>
-
             <div className="border-t my-2 border-gray-100"></div>
-
-            <div className="bg-blue-600 text-white rounded px-4 py-2 text-sm font-medium w-full hover:bg-blue-700 transition flex items-center gap-2 justify-center">
+            <div
+              onClick={handleSignOut}
+              className="bg-blue-600 text-white rounded px-4 py-2 text-sm font-medium w-full hover:bg-blue-700 transition flex items-center gap-2 justify-center cursor-pointer"
+            >
               <SignOut size={20} className="text-white" />
               <p className=" font-normal text-white">Sign Out</p>
             </div>
