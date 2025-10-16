4677a3267653e1ae19e18bf0b83e79194fddcd35
Israel-sanmi <ilesanmiisrael@outlook.com>
2025-10-16 22:54:20 +0100

    reactions

--- app/page.tsx
+++ app/page.tsx
@@ -3,37 +3,139 @@ import Image from "next/image";
 import Navbar from "@/components/main/landing-page/navbar/navbar";
 import ActiveUsers from "@/components/main/landing-page/sidebar/activeUsers";
 import Suggestions from "@/components/main/landing-page/sidebar/suggestions";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ReactionModal from "@/components/main/modal/reacttionModal";
 import AddPostModal from "@/components/main/modal/addPostModal";
 import { FaHandsClapping } from "react-icons/fa6";
 import { HiOutlineLightBulb } from "react-icons/hi";
 import { CgSmileMouthOpen } from "react-icons/cg";
-import { IoHeartCircle } from "react-icons/io5";
+import { IoClose, IoHeartCircle } from "react-icons/io5";
 import { PiHandHeartFill } from "react-icons/pi";
 
 import { BiCommentDetail } from "react-icons/bi";
-import { BiRepost } from "react-icons/bi";
 import { LuSend } from "react-icons/lu";
 import { FaHeart, FaThumbsUp } from "react-icons/fa";
 import { TbDots } from "react-icons/tb";
 import StoriesLanding from "@/components/main/landing-page/index/stories";
 import SimilarProfiles from "@/components/main/profile/side-card/similarProfiles";
-import LinkedInPostFeed from "@/components/main/atom/feedMessage";
-import { motion, AnimatePresence } from "framer-motion";
+import { BiRepost } from "react-icons/bi";
+import { FiEdit } from "react-icons/fi";
+import ReactionPopup from "@/components/main/landing-page/reactionHover";
+import SendModal from "@/components/main/modal/sendModal";
+
+interface User {
+  id: number;
+  name: string;
+  role: string;
+  avatar: string;
+}
+
+const users: User[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    role: "Developer",
+    avatar: "/images/user/userImg.jpg",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    role: "Designer",
+    avatar: "/images/user/userImg.jpg",
+  },
+  {
+    id: 3,
+    name: "Alice Johnson",
+    role: "Manager",
+    avatar: "/images/user/userImg.jpg",
+  },
+  {
+    id: 4,
+    name: "Michael Brown",
+    role: "Developer | Team Lead",
+    avatar: "/images/user/userImg.jpg",
+  },
+  {
+    id: 5,
+    name: "Emily Davis",
+    role: "Designer | UI/UX Specialist",
+    avatar: "/images/user/userImg.jpg",
+  },
+  {
+    id: 6,
+    name: "Robert Wilson",
+    role: "Manager | Product Owner",
+    avatar: "/images/user/userImg.jpg",
+  },
+  {
+    id: 7,
+    name: "Sophia Martinez",
+    role: "Developer | Backend Engineer",
+    avatar: "/images/user/userImg.jpg",
+  },
+  {
+    id: 8,
+    name: "David Lee",
+    role: "QA Engineer | Automation Tester",
+    avatar: "/images/user/userImg.jpg",
+  },
+];
 
 export default function Home() {
   const reactions = [
-    { icon: <FaThumbsUp className="w-5 h-5"/>, bg: "bg-blue-400" },
-    { icon: <FaHandsClapping className="w-5 h-5"/>, bg: "bg-green-400" },
-    { icon: <PiHandHeartFill className="w-5 h-5"/>, bg: "bg-purple-400" },
-    { icon: <IoHeartCircle className="w-5 h-5"/>, bg: "bg-red-400" },
-    { icon: <HiOutlineLightBulb className="w-5 h-5"/>, bg: "bg-yellow-400" },
-    { icon: <CgSmileMouthOpen className="w-5 h-5"/>, bg: "bg-green-400" },
+    { icon: <FaThumbsUp className="w-5 h-5" />, bg: "bg-blue-400" },
+    { icon: <FaHandsClapping className="w-5 h-5" />, bg: "bg-green-400" },
+    { icon: <PiHandHeartFill className="w-5 h-5" />, bg: "bg-purple-400" },
+    { icon: <IoHeartCircle className="w-5 h-5" />, bg: "bg-red-400" },
+    { icon: <HiOutlineLightBulb className="w-5 h-5" />, bg: "bg-yellow-400" },
+    { icon: <CgSmileMouthOpen className="w-5 h-5" />, bg: "bg-green-400" },
   ];
 
   const [openLikesModal, setOpenLikesModal] = useState(false);
-  const [likesHoverFn, setLikesHoverFn] = useState(false);
+  const [showReactions, setShowReactions] = useState(false);
+  const hideTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const handleMouseEnter = () => {
+    if (hideTimeout.current) {
+      clearTimeout(hideTimeout.current);
+      hideTimeout.current = null;
+    }
+    setShowReactions(true);
+  };
+
+  const handleMouseLeave = () => {
+    hideTimeout.current = setTimeout(() => {
+      setShowReactions(false);
+    }, 500);
+  };
+
+  const [openRepostModal, setOpenRepostModal] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setOpenRepostModal(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedUsers, setSelectedUsers] = useState<number[]>([]);
+
+  const toggleUser = (id: number) => {
+    setSelectedUsers((prev) =>
+      prev.includes(id) ? prev.filter((u) => u !== id) : [...prev, id]
+    );
+  };
 
   return (
     <main className="main-content pb-20">
@@ -135,46 +237,19 @@ export default function Home() {
 
                             <div className="flex relative items-center justify-between flex-1 max-w-[500px] sm:max-w-[600px] md:max-w-[700px] lg:max-w-none">
                               <div
-                                onMouseEnter={() => setLikesHoverFn(true)}
-                                onMouseLeave={() => setLikesHoverFn(false)}
-                                className="flex flex-col cursor-pointer items-center gap-1"
+                                className="relative flex flex-col items-center"
+                                onMouseEnter={handleMouseEnter}
+                                onMouseLeave={handleMouseLeave}
                               >
-                                <FaThumbsUp className="text-gray-600 text-lg sm:text-xl" />
-                                <p className="font-bold text-xs sm:text-sm md:text-base">
-                                  Like
-                                </p>
+                                <div className="flex flex-col cursor-pointer items-center gap-1">
+                                  <FaThumbsUp className="text-gray-600 text-lg sm:text-xl" />
+                                  <p className="font-bold text-xs sm:text-sm md:text-base">
+                                    Like
+                                  </p>
+                                </div>
+                                <ReactionPopup show={showReactions} />
                               </div>
-                              {likesHoverFn && (
-                                <AnimatePresence>
-                                  <motion.div
-                                    className="absolute flex items-center justify-between gap-2 -top-14 bg-white rounded-full p-2 shadow-md"
-                                    initial={{ opacity: 0, y: 20 }}
-                                    animate={{ opacity: 1, y: 0 }}
-                                    exit={{ opacity: 0, y: 20 }}
-                                  >
-                                    {reactions.map((reaction, index) => (
-                                      <motion.div
-                                        key={index}
-                                        className={`${reaction.bg} flex justify-center items-center text-white p-2 rounded-full cursor-pointer`}
-                                        initial={{ y: 20, scale: 0.5 }}
-                                        animate={{ y: 0, scale: 1 }}
-                                        transition={{
-                                          delay: index * 0.1,
-                                          type: "spring",
-                                          stiffness: 500,
-                                        }}
-                                        whileHover={{
-                                          scale: 1.5,
-                                          y: -10,
-                                          zIndex: 50,
-                                        }}
-                                      >
-                                        {reaction.icon}
-                                      </motion.div>
-                                    ))}
-                                  </motion.div>
-                                </AnimatePresence>
-                              )}
+
                               <div className="flex flex-col items-center gap-1">
                                 <BiCommentDetail className="text-gray-600 text-lg sm:text-xl" />
                                 <p className="font-bold text-xs sm:text-sm md:text-base">
@@ -182,24 +257,80 @@ export default function Home() {
                                 </p>
                               </div>
 
-                              <div className="flex flex-col items-center gap-1">
-                                <BiRepost className="text-gray-600 text-lg sm:text-xl" />
-                                <p className="font-bold text-xs sm:text-sm md:text-base">
-                                  Repost
-                                </p>
+                              <div className="relative flex flex-col items-center gap-1">
+                                <div
+                                  onClick={() =>
+                                    setOpenRepostModal((prev) => !prev)
+                                  }
+                                  className="flex cursor-pointer flex-col items-center gap-1"
+                                >
+                                  <BiRepost className="text-gray-600 text-lg sm:text-xl" />
+                                  <p className="font-bold text-xs sm:text-sm md:text-base">
+                                    Repost
+                                  </p>
+                                </div>
+
+                                {openRepostModal && (
+                                  <div
+                                    ref={dropdownRef}
+                                    className="absolute top-full mt-1 bg-white space-y-2 w-[300px] p-2 rounded-sm shadow-md z-50"
+                                  >
+                                    <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 p-2 rounded">
+                                      <FiEdit className="flex-none" size={22} />
+                                      <div>
+                                        <h3 className="font-medium text-sm">
+                                          Repost with your thoughts
+                                        </h3>
+                                        <p className="font-normal text-xs">
+                                          Create a new post with Salem's post
+                                          attached
+                                        </p>
+                                      </div>
+                                    </div>
+                                    <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 p-2 rounded">
+                                      <BiRepost
+                                        className="flex-none"
+                                        size={22}
+                                      />
+                                      <div>
+                                        <h3 className="font-medium text-sm">
+                                          Repost
+                                        </h3>
+                                        <p className="font-normal text-xs">
+                                          Instantly bring Salem's post to others
+                                          feed
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                )}
                               </div>
 
-                              <div className="flex flex-col items-center gap-1">
-                                <LuSend className="text-gray-600 text-lg sm:text-xl" />
-                                <p className="font-bold text-xs sm:text-sm md:text-base">
-                                  Send
-                                </p>
+                              <div>
+                                <div
+                                  className="flex flex-col items-center gap-1 cursor-pointer"
+                                  onClick={() => setOpenModal(true)}
+                                >
+                                  <LuSend className="text-gray-600 text-lg sm:text-xl" />
+                                  <p className="font-bold text-xs sm:text-sm md:text-base">
+                                    Send
+                                  </p>
+                                </div>
+
+                                {openModal && (
+                                  <SendModal
+                                    users={users}
+                                    selectedUsers={selectedUsers}
+                                    toggleUser={toggleUser}
+                                    setOpenModal={setOpenModal}
+                                    setSelectedUsers={setSelectedUsers}
+                                  />
+                                )}
                               </div>
                             </div>
                           </div>
                         </div>
                       ))}
-                      {/* <LinkedInPostFeed/> */}
                       <div className="rounded-lg p-5 w-full bg-white shadow-md mb-[40px]">
                         <div className="flex items-center justify-between">
                           <div className="flex items-center gap-2">
--- components/main/landing-page/reactionHover.tsx
+++ components/main/landing-page/reactionHover.tsx
@@ -1,58 +1,57 @@
-// import { useState, useRef } from "react";
-// import { AnimatePresence, motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { FaHandsClapping, FaThumbsUp } from "react-icons/fa6";
+import { HiOutlineLightBulb } from "react-icons/hi";
+import { CgSmileMouthOpen } from "react-icons/cg";
+import { IoHeartCircle } from "react-icons/io5";
+import { PiHandHeartFill } from "react-icons/pi";
 
-// export default function ReactionHover() {
-//   const [showReactions, setShowReactions] = useState(false);
-//   const hideTimeout = useRef<NodeJS.Timeout | null>(null);
+const reactions = [
+  { icon: <FaThumbsUp className="w-5 h-5" />, bg: "bg-blue-400" },
+  { icon: <FaHandsClapping className="w-5 h-5" />, bg: "bg-green-400" },
+  { icon: <PiHandHeartFill className="w-5 h-5" />, bg: "bg-purple-400" },
+  { icon: <IoHeartCircle className="w-5 h-5" />, bg: "bg-red-400" },
+  { icon: <HiOutlineLightBulb className="w-5 h-5" />, bg: "bg-yellow-400" },
+  { icon: <CgSmileMouthOpen className="w-5 h-5" />, bg: "bg-green-400" },
+];
 
-//   const handleMouseEnter = () => {
-//     if (hideTimeout.current) {
-//       clearTimeout(hideTimeout.current);
-//       hideTimeout.current = null;
-//     }
-//     setShowReactions(true);
-//   };
-
-//   const handleMouseLeave = () => {
-//     hideTimeout.current = setTimeout(() => {
-//       setShowReactions(false);
-//   };
-
-//   return (
-//     <div
-//       className="relative"
-//       onMouseEnter={handleMouseEnter}
-//       onMouseLeave={handleMouseLeave}
-//     >
-//       <button className="p-2 bg-gray-200 rounded">Like</button>
-
-//       <AnimatePresence>
-//         {showReactions && (
-//           <motion.div
-//             className="absolute flex items-center justify-between gap-2 -top-14 bg-white rounded-full p-2 shadow-md"
-//             initial={{ opacity: 0, y: 20 }}
-//             animate={{ opacity: 1, y: 0 }}
-//             exit={{ opacity: 0, y: 20 }}
-//           >
-//             {reactions.map((reaction, index) => (
-//               <motion.div
-//                 key={index}
-//                 className={`${reaction.bg} flex justify-center items-center text-white p-2 rounded-full cursor-pointer`}
-//                 initial={{ y: 20, scale: 0.5 }}
-//                 animate={{ y: 0, scale: 1 }}
-//                 transition={{
-//                   delay: index * 0.1,
-//                   type: "spring",
-//                   stiffness: 500,
-//                 }}
-//                 whileHover={{ scale: 1.5, y: -10, zIndex: 50 }}
-//               >
-//                 {reaction.icon}
-//               </motion.div>
-//             ))}
-//           </motion.div>
-//         )}
-//       </AnimatePresence>
-//     </div>
-//   );
-// }
+export default function ReactionPopup({
+  show,
+  onHoverStart,
+  onHoverEnd,
+}: {
+  show: boolean;
+  onHoverStart?: () => void;
+  onHoverEnd?: () => void;
+}) {
+  return (
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          className="absolute z-[999] flex items-center justify-between gap-2 -top-14 bg-white rounded-full p-1.5 shadow-md"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 20 }}
+          onMouseEnter={onHoverStart}
+          onMouseLeave={onHoverEnd}
+        >
+          {reactions.map((reaction, index) => (
+            <motion.div
+              key={index}
+              className={`${reaction.bg} flex justify-center items-center text-white p-1.5 rounded-full cursor-pointer`}
+              initial={{ y: 20, scale: 0.5 }}
+              animate={{ y: 0, scale: 1 }}
+              transition={{
+                delay: index * 0.1,
+                type: "spring",
+                stiffness: 500,
+              }}
+              whileHover={{ scale: 1.5, y: -10, zIndex: 50 }}
+            >
+              {reaction.icon}
+            </motion.div>
+          ))}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
